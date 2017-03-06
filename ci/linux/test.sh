2ec180cfc29dd9934536d9147d7e5d2f65bbfdbb
Ashley Mannix <ashleymannix@live.com.au>
2017-03-07 08:26:10 +1000

    update dockerfile

--- ci/linux/test.sh
+++ ci/linux/test.sh
@@ -1,2 +1,2 @@
 docker build -t nugetrs/build:latest ci/linux/
-docker run nugetrs/build:latest /bin/bash -v $(pwd):/src -c 'cd /src && ./ci/test.sh'
+docker run -v $(pwd):/src -w /src nugetrs/build:latest /bin/bash -c './ci/test.sh'
