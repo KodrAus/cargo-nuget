cf980563970ceadebdb4bf2e9ebdb9c51289cdd6
Ashley Mannix <ashleymannix@live.com.au>
2017-03-08 09:30:47 +1000

    make docker build local

--- ci/linux/test.sh
+++ ci/linux/test.sh
@@ -1,4 +1,4 @@
 set -e
 
-docker build --build-arg RUST_VERSION=$RUST_VERSION -t nugetrs/build:latest ci/linux/
-docker run -v $(pwd):/src -w /src nugetrs/build:latest /bin/bash -c './ci/test.sh'
+docker build --build-arg RUST_VERSION=$RUST_VERSION -t nugetrs-build:latest ci/linux/
+docker run -v $(pwd):/src -w /src nugetrs-build:latest /bin/bash -c './ci/test.sh'
