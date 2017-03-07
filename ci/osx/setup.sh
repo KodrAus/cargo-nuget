f84e99a89e75d1e138379fafa76654f45067f456
Ashley Mannix <ashleymannix@live.com.au>
2017-03-07 13:39:40 +1000

    update rust build

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -1,7 +1,8 @@
 set -e
 
 DOTNET_SDK_DOWNLOAD_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VERSION/dotnet-dev-osx-x64.$DOTNET_SDK_VERSION.tar.gz
-RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/rust-$RUST_VERSION-x86_64-apple-darwin.tar.gz
+RUST_ARCHIVE=rust-$RUST_VERSION-x86_64-apple-darwin.tar.gz
+RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/$RUST_ARCHIVE
 
 # Install the .NET SDK
 curl -L $DOTNET_SDK_DOWNLOAD_URL --output dotnet.tar.gz
@@ -10,6 +11,7 @@ tar zxf dotnet.tar.gz -C $HOME/dotnet
 export PATH=$HOME/dotnet:$PATH
 
 # Install Rust
-curl -L $RUST_DOWNLOAD_URL --output rust.tar.gz
-tar zxf rust.tar.gz
+curl -fsOSL $RUST_DOWNLOAD_URL
+curl -s $RUST_DOWNLOAD_URL.sha256 | sha256sum -c - tar -C /rust -xzf $RUST_ARCHIVE --strip-components=1
+rm $RUST_ARCHIVE
 ./install.sh
