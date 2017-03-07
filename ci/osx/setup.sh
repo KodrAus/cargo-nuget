ce698fd709ddd4abecc8e7a331c595ba63f689a5
Ashley Mannix <ashleymannix@live.com.au>
2017-03-07 11:42:38 +1000

    stricter errors

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -1,3 +1,5 @@
+set -e
+
 DOTNET_SDK_DOWNLOAD_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VERSION/dotnet-dev-osx-x64.$DOTNET_SDK_VERSION.tar.gz
 RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/rust-$RUST_VERSION-x86_64-apple-darwin.tar.gz
 
--- ci/osx/test.sh
+++ ci/osx/test.sh
@@ -1,1 +1,3 @@
+set -e
+
 ./ci/test.sh
