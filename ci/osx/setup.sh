6663bf4b6c2798e658b8cda90e601fec3e8ba63e
Ashley Mannix <ashleymannix@live.com.au>
2017-03-13 14:28:37 +1000

    update osx dotnet version

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -1,6 +1,7 @@
 set -e
 
 DOTNET_SDK_DOWNLOAD_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VERSION/dotnet-dev-osx-x64.$DOTNET_SDK_VERSION.tar.gz
+
 RUST_ARCHIVE=rust-$RUST_VERSION-x86_64-apple-darwin
 RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/$RUST_ARCHIVE.tar.gz
 
