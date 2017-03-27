dabaf85f5e8ee408fdf2a9a3c189a099fc97c9e0
Ashley Mannix <ashleymannix@live.com.au>
2017-03-27 12:44:33 +1000

    use dotnet install script

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -1,14 +1,14 @@
 set -e
 
-DOTNET_SDK_DOWNLOAD_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VERSION/dotnet-dev-osx-x64.$DOTNET_SDK_VERSION.tar.gz
+DOTNET_SDK_DOWNLOAD_URL=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
 
 RUST_ARCHIVE=rust-$RUST_VERSION-x86_64-apple-darwin
 RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/$RUST_ARCHIVE.tar.gz
 
 # Install the .NET SDK
+mkdir ./build/
 curl -L $DOTNET_SDK_DOWNLOAD_URL --output dotnet.tar.gz
-mkdir $HOME/dotnet
-tar zxf dotnet.tar.gz -C $HOME/dotnet
+./build/installcli.ps1 -InstallDir $HOME/dotnet -NoPath -Version 1.0.1
 
 ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
 ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
---
+++ ci/win/setup.ps1
@@ -0,0 +1,4 @@
+mkdir tmp
+cd tmp
+
+Invoke-WebRequest https://static.rust-lang.org/dist/rust-1.15.1-x86_64-pc-windows-msvc.tar.gz -OutFile .\tmp\rust-setup.tar.gz
