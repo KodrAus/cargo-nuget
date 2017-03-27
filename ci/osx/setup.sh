ce4519e9af332e72e733789c3baef5c854ebead4
Ashley Mannix <ashleymannix@live.com.au>
2017-03-27 14:11:45 +1000

    set right output on setup

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -7,7 +7,7 @@ RUST_DOWNLOAD_URL=https://static.rust-lang.org/dist/$RUST_ARCHIVE.tar.gz
 
 # Install the .NET SDK
 mkdir ./build/
-curl -L $DOTNET_SDK_DOWNLOAD_URL --output dotnet.tar.gz
+curl -L $DOTNET_SDK_DOWNLOAD_URL --output ./build/installcli.sh
 ./build/installcli.sh -InstallDir $HOME/dotnet -NoPath -Version $DOTNET_SDK_VERSION
 
 ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
